27e1ac1978d0ca0b6c13245366d6c425db96b8c2
TrueXPixels <truexpixels@gmail.com>
2018-10-17 22:05:59 -0700

    Initial Commit

---
+++ MoreJoinMessages/src/me/truexpixels/morejoinmessages/Main.java
@@ -0,0 +1,14 @@
+package me.truexpixels.morejoinmessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.truexpixels.morejoinmessages.join.JoinListener;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		new JoinListener(this);
+	}
+	
+}
---
+++ MoreJoinMessages/src/me/truexpixels/morejoinmessages/utils/Utils.java
@@ -0,0 +1,11 @@
+package me.truexpixels.morejoinmessages.utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+	
+	public static String chat (String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+	
+}
